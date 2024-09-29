875e508b7e912486d79aa957195180aba910e2e7
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2024-09-29 17:13:04 +0700

    Add image validation

---
+++ EcommerceMVC/Repository/Validations/FileExtensionAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceMVC.Repository.Validations;
+
+public class FileExtensionAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is IFormFile file)
+        {
+            var extension = Path.GetExtension(file.FileName);
+            string[] validExtensions = { ".jpg", ".jpeg", ".png" };
+            if (!validExtensions.Contains(extension.ToLower()))
+            {
+                return new ValidationResult("Invalid file extension. Please upload a valid file.");
+            }
+        }
+        
+        return ValidationResult.Success;
+    }
+}
